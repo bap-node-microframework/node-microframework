2bb6b1071f220bf3ac6ce903c8767a6b3b2f4cdd
Cedric LOMBARDOT <cedric.lombardot@gmail.com>
2016-02-19 12:54:48 +0100

    Export decorators

---
+++ dist/decorators.d.ts
@@ -0,0 +1,4 @@
+export * from "./decorators/events.decorators";
+export * from "./decorators/paramconverter.decorators";
+export * from "./decorators/routing.decorators";
+export * from "./decorators/security.decorators";
---
+++ dist/decorators.js
@@ -0,0 +1,9 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+__export(require("./decorators/events.decorators"));
+__export(require("./decorators/paramconverter.decorators"));
+__export(require("./decorators/routing.decorators"));
+__export(require("./decorators/security.decorators"));
+//# sourceMappingURL=decorators.js.map
