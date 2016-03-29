af8505af3fd186ddffe60a8442e1db95746e4a41
JulesTruong <jules.truong.pro@gmail.com>
2016-03-29 18:17:57 +0200

    with tsc

---
+++ dist/decorators/queryparam.decorators.d.ts
@@ -0,0 +1,1 @@
+export declare function QueryParam(...getArgs: any[]): (target: any, name: any, descriptor: any) => void;
---
+++ dist/decorators/requestparam.decorators.js
@@ -0,0 +1,29 @@
+"use strict";
+var _ = require("lodash");
+function RequestParam() {
+    var getArgs = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        getArgs[_i - 0] = arguments[_i];
+    }
+    var aName = getArgs[0];
+    var aType = getArgs[1];
+    var isNullable = getArgs[2];
+    return function validator(target, name, descriptor) {
+        var oldValue = descriptor.value;
+        descriptor.value = function (req, res) {
+            if (!isNullable && !req.param[aName]) {
+                return res.status(400).json({
+                    error: "parameter " + aName + " is required"
+                });
+            }
+            if (!isNullable && !_[aType](req.param[aName])) {
+                return res.status(400).json({
+                    error: "parameter " + aName + " should typeof " + aType
+                });
+            }
+            return oldValue(req, res);
+        };
+    };
+}
+exports.RequestParam = RequestParam;
+//# sourceMappingURL=requestparam.decorators.js.map
