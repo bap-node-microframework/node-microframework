be732adb0d9e5ef5909dcda4fc9e952c5760d412
JulesTruong <jules.truong.pro@gmail.com>
2016-03-30 14:43:00 +0200

    format queryparam and request param

--- dist/decorators/requestparam.decorators.js
+++ dist/decorators/requestparam.decorators.js
@@ -6,20 +6,27 @@ function RequestParam() {
         getArgs[_i - 0] = arguments[_i];
     }
     var aName = getArgs[0];
-    var aType = getArgs[1];
-    var isNullable = getArgs[2];
+    var options = getArgs[1];
+    var isNullable = options.nullable || false;
+    var defaultValue = options.default || null;
+    var requirements = options.requirements || null;
     return function validator(target, name, descriptor) {
         var oldValue = descriptor.value;
         descriptor.value = function (req, res) {
+            // param not nullable and null => error
             if (!isNullable && !req.param[aName]) {
                 return res.status(400).json({
                     error: "parameter " + aName + " is required"
                 });
             }
-            if (!isNullable && !_[aType](req.param[aName])) {
+            // param not nullable and not valid => error
+            if (!isNullable && !_[requirements](req.query[aName])) {
                 return res.status(400).json({
-                    error: "parameter " + aName + " should typeof " + aType
+                    error: "parameter " + aName + " should typeof " + requirements
                 });
+            }
+            if (isNullable && !req.query[aName]) {
+                req.query[aName] = defaultValue;
             }
             return oldValue(req, res);
         };
--- src/decorators/requestparam.decorators.ts
+++ src/decorators/requestparam.decorators.ts
@@ -5,24 +5,32 @@ import * as _  from "lodash";
 export function RequestParam(...getArgs) {
 
     let aName = getArgs[0];
-    let aType = getArgs[1];
-    let isNullable = getArgs[2];
+    let options = getArgs[1];
+    let isNullable = options.nullable || false;
+    let defaultValue = options.default || null;
+    let requirements = options.requirements || null;
 
     return function validator(target, name, descriptor) {
         let oldValue = descriptor.value;
 
         descriptor.value = (req, res) => {
 
+            // param not nullable and null => error
             if (!isNullable && !req.param[aName]) {
                 return res.status(400).json({
                     error: "parameter " + aName + " is required"
                 });
             }
 
-            if (!isNullable && !_[aType](req.param[aName])) {
+            // param not nullable and not valid => error
+            if (!isNullable && !_[requirements](req.query[aName])) {
                 return res.status(400).json({
-                    error: "parameter " + aName + " should typeof " + aType
+                    error: "parameter " + aName + " should typeof " + requirements
                 });
+            }
+
+            if (isNullable && !req.query[aName]) {
+                req.query[aName] = defaultValue;
             }
 
             return oldValue(req, res);
