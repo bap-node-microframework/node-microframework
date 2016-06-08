55ac48bc571e13b324266237513ede09f6426ffe
JulesTruong <jules.truong.pro@gmail.com>
2016-06-08 10:37:12 +0200

    add possibility to format date of log file

--- dist/core/logger.js
+++ dist/core/logger.js
@@ -15,7 +15,8 @@ var Logger = (function () {
         var accessLogStream = FileStreamRotator.getStream({
             filename: logDirectory + path.sep + (config.get('log.file_format') || 'access-%DATE%.log'),
             frequency: config.get('log.rotate.frequency') || 'daily',
-            verbose: config.get('log.verbose') || false
+            verbose: config.get('log.verbose') || false,
+            date_format: config.get('log.date_format') || 'YYYYMMDD'
         });
         var skip = function () { return false; };
         if (config.get('log.skip') !== 'none') {
